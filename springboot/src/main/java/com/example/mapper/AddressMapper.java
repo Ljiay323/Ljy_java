7fb596500a059b89614761da099a275258e2e26f
Ljiay323 <ljiay0203@outlook.com>
2024-12-10 11:41:10 +0800

    提交文件

---
+++ springboot/src/main/java/com/example/mapper/AddressMapper.java
@@ -0,0 +1,40 @@
+package com.example.mapper;
+
+import com.example.entity.Address;
+import com.example.entity.Collect;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * 操作address相关数据接口
+*/
+public interface AddressMapper {
+
+    /**
+     * 新增
+     */
+    int insert(Address address);
+
+    /**
+     * 删除
+     */
+    int deleteById(Integer id);
+
+    /**
+     * 修改
+     */
+    int updateById(Address address);
+
+    /**
+     * 根据ID查询
+     */
+    Address selectById(Integer id);
+
+    /**
+     * 查询所有
+     */
+    List<Address> selectAll(Address address);
+
+}
